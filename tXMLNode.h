106e6dcfaf5a9474b189e3d7d96e36c5b23c5b58
Tobias Föhst <foehst@informatik.uni-kl.de>
2011-10-07 17:42:27 +0200

    Updated and added old/missing copyright notes

--- tXMLNode.h
+++ tXMLNode.h
@@ -2,7 +2,7 @@
 // You received this file as part of RRLib
 // Robotics Research Library
 //
-// Copyright (C) AG Robotersysteme TU Kaiserslautern
+// Copyright (C) Finroc GbR (finroc.org)
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
